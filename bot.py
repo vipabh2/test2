e18aa8ce4d234dde5832ad612ccc7416191eba99
VIPABH <a891983.2@gmail.com>
2024-12-02 05:07:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,16 @@ import os
 import telebot
 
 # استدعاء التوكن من المتغير البيئي
-bot_token = os.getenv("BOT_TOKEN")  # BOT_TOKEN هو اسم الـ Secret الذي أضفته في GitHub
+bot_token = os.getenv("BOT_TOKEN")
+
 if not bot_token:
     raise ValueError("Missing BOT_TOKEN environment variable")
 
 bot = telebot.TeleBot(bot_token)
 
-@bot.message_handler(func=lambda message: message.text in ['المزيد'])
-def more(message):
-    bot.reply_to(message, """
-    ~ الالعاب المتوفره ~
-\n •الكت تويت والأمر الخاص بيها `كتويت`
-\n •الأرقام ، احزر الرقم المطلوب والأمر الخاص بيها /num 
-\n •امر الميمز ، `ميم` او `ميمز` يرسلك صورة ميم 
-\n \n استمتع ❤️
-""", parse_mode='Markdown')
+# مثال لرسالة مبدئية أو تفاعل مع البوت
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, welcome to the bot!")
+
+bot.polling()

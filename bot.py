4cd254a961c6e938fd0890e6537b20f8dc0d2706
VIPABH <a891983.2@gmail.com>
2024-12-08 23:34:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,18 +14,7 @@ joker = ["🤔", "🙄", "😳", "🥲", "😜"]
 correct_answer = None  
 banned_users = []  
 
-def format_board(game_board, numbers_board):
-    formatted_board = ""
-    formatted_board += " ".join(numbers_board[0]) + "\n"
-    formatted_board += " ".join(game_board[0]) + "\n"
-    return formatted_board
 
-def reset_game(chat_id):
-    global game_board, correct_answer, group_game_status, correct_answer 
-    game_board = [row[:] for row in original_game_board]
-    correct_answer = None
-    group_game_status[chat_id]['is_game_started2'] = False
-    group_game_status[chat_id]['joker_player'] = None
 
 @bot.message_handler(func=lambda message: message.text == 'محيبس')
 def strt(message):
@@ -46,12 +35,32 @@ def strt(message):
     if chat_id not in group_game_status:
         group_game_status[chat_id] = {'is_game_started2': False, 'joker_player': None}
 
+@bot.callback_query_handler(func=lambda call: call.data == "start_game")
+def handle_start_game(call):
+    chat_id = call.message.chat.id
+    user_id = call.from_user.id 
+    correct_answer = random.randint(1, 6)  
+
     if not group_game_status[chat_id]['is_game_started2']:
         group_game_status[chat_id]['is_game_started2'] = True
         group_game_status[chat_id]['joker_player'] = None
-        correct_answer = random.randint(1, 2)  
-        bot.reply_to(message, f"تم بدء اللعبة! لفتح العضمة ارسل 'طك رقم العضمة' للمشاركة.")
+        correct_answer = random.randint(1, 6)  
 
+def format_board(game_board, numbers_board):
+    """تنسيق الجدول للعرض بشكل مناسب"""
+    formatted_board = ""
+    formatted_board += " ".join(numbers_board[0]) + "\n"
+    formatted_board += " ".join(game_board[0]) + "\n"
+    return formatted_board
+
+
+def reset_game(chat_id):
+    """إعادة تعيين حالة اللعبة بعد انتهائها"""
+    global game_board, correct_answer, group_game_status
+    game_board = [row[:] for row in original_game_board]
+    correct_answer = None
+    group_game_status[chat_id]['is_game_started2'] = False
+    group_game_status[chat_id]['joker_player'] = None
 
 
 @bot.message_handler(regexp=r'\طك (\d+)')
@@ -81,20 +90,7 @@ def handle_strike(message):
         except (IndexError, ValueError):
             bot.reply_to(message, "يرجى إدخال رقم صحيح بين 1 و 6.")
 
-@bot.message_handler(regexp=r'\انا')
-def handle_incoming_message(message):
-    chat_id = message.chat.id
-    if chat_id not in group_game_status:
-        group_game_status[chat_id] = {'is_game_started2': False, 'joker_player': None}
-    if group_game_status[chat_id]['is_game_started2'] and not group_game_status[chat_id]['joker_player']:
-        group_game_status[chat_id]['joker_player'] = message.from_user.id
-
-@bot.callback_query_handler(func=lambda call: call.data == "start_game")
-def handle_start_game(call):
-    chat_id = call.message.chat.id
-    correct_answer = random.randint(1, 6)  
     bot.send_message(chat_id, f"تم اختيار الرقم السري! اللعبة جاهزة. لفتح العضمة أرسل 'طك <رقم>'.")
-
 @bot.message_handler(regexp=r'\جيب (\d+)')
 def handle_guess(message):
     """التعامل مع التخمينات من قبل اللاعبين"""
@@ -106,19 +102,17 @@ def handle_guess(message):
             guess = int(message.text.split()[1])
             if 1 <= guess <= 6:
                 if guess == correct_answer:
-                    # إذا كان التخمين صحيحًا
                     winner_id = message.from_user.id
                     points[winner_id] = points.get(winner_id, 0) + 1
                     sender_first_name = message.from_user.first_name
                     game_board = [["💍" if i == correct_answer - 1 else "🖐️" for i in range(6)]]
                     bot.send_message(chat_id, f'🎉 الف مبروك! اللاعب ({sender_first_name}) وجد المحبس 💍!\n{format_board(game_board, numbers_board)}')
-                    reset_game(chat_id)  # إعادة تعيين اللعبة
+                    reset_game(chat_id) 
                 else:
-                    # إذا كان التخمين خاطئًا
                     sender_first_name = message.from_user.first_name
                     game_board = [["❌" if i == guess - 1 else "🖐️" for i in range(6)]]
                     bot.send_message(chat_id, f"❌ اللاعب ({sender_first_name}) خسر اللعبة! المحبس لم يكن هنا.\n{format_board(game_board, numbers_board)}")
-                    reset_game(chat_id)  # إعادة تعيين اللعبة
+                    reset_game(chat_id) 
             else:
                 bot.reply_to(message, "❗ يرجى إدخال رقم صحيح بين 1 و 6.")
         except (IndexError, ValueError):

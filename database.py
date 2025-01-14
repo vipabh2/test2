bc624a4d85a2f01b9461b3bbf66800b45830b2d9
VIPABH <a891983.2@gmail.com>
2025-01-14 19:23:56 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 BASE = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

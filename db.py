c4abd738052aa0db2390334dda8255c12902438a
VIPABH <a891983.2@gmail.com>
2025-01-14 21:08:30 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from db import BASE, SESSION, engine
 import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')

e6f13347f4c1d25f3894c58c85f393d88ac09509
VIPABH <a891983.2@gmail.com>
2025-01-19 23:59:57 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,9 +9,9 @@ api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
-@ABH.on(events.NewMessage)
-async def handle_new_message(event):
-    await event.reply("ها هههههه")
+@ABH.on(events.MessageEdited)
+async def handle_edited_message(event):
+    await event.reply("تم تعديل هذه الرسالة")
     pass
 
 ABH.run_until_disconnected()

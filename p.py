542dad572b15882a15e7e623cdb48cc1a5dcd0ec
VIPABH <a891983.2@gmail.com>
2025-01-19 23:58:40 +0300

    Update p.py

--- p.py
+++ p.py
@@ -3,14 +3,15 @@ import requests, os, operator, asyncio, random
 from googletrans import Translator
 from bs4 import BeautifulSoup
 import time
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
-@ABH.on(events.NewMessage(edit=True))
+
+@ABH.on(events.NewMessage)
 async def handle_new_message(event):
     await event.reply("ها هههههه")
     pass
 
 ABH.run_until_disconnected()
-

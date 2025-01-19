bc1045e15fcabf054514da908de088157b156d5b
VIPABH <a891983.2@gmail.com>
2025-01-20 00:03:42 +0300

    Update p.py

--- p.py
+++ p.py
@@ -11,7 +11,8 @@ ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
 @ABH.on(events.MessageEdited)
 async def handle_edited_message(event):
-    await event.reply("ها هههههه")
+    if event.message.media:  # Check if the message has media (files, photos, videos)
+        await event.reply("تم تعديل هذه الرسالة التي تحتوي على ملفات أو صور أو فيديوهات")
     pass
 
 ABH.run_until_disconnected()

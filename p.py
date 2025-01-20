66df1bb5ebfb4b92a022323ebbd39f8ccb7bf982
VIPABH <a891983.2@gmail.com>
2025-01-20 14:31:48 +0300

    Update p.py

--- p.py
+++ p.py
@@ -12,22 +12,27 @@ ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
 @ABH.on(events.NewMessage(pattern=r'^اضف كروب (\d+)$'))
 async def add_group(event):
-    match = event.pattern_match  # استخراج الرقم من الأمر
+    match = event.pattern_match
     if match:
         notification_group_id = int(match.group(1))
-        group_id = event.chat_id  # الحصول على معرف المجموعة الحالية
-        save_notification_group(group_id, notification_group_id)  # تخزين في قاعدة البيانات
-        await event.reply(f"تم تعيين الكروب بمعرف: {notification_group_id} ككروب التبليغ.")
+        group_id = event.chat_id
+        chat_link = await event.get_chat()
+        chat_link = chat_link.username if chat_link.username else None
+        if chat_link:
+            chat_link = f"https://t.me/{chat_link}"
+        else:
+            chat_link = "رابط غير متاح"
+        save_notification_group(group_id, notification_group_id, chat_link)
+        await event.reply(f"تم تعيين الكروب بمعرف: {notification_group_id} ككروب التبليغ. رابط المجموعة: {chat_link}")
     else:
         await event.reply("يرجى إدخال معرف كروب صحيح. مثال: `اضف كروب 123456789`")
 
 @ABH.on(events.NewMessage(pattern=r'^احذف كروب (\d+)$')) 
 async def delete_group(event):
-    match = event.pattern_match  # استخراج الرقم من الأمر
+    match = event.pattern_match
     if match:
         notification_group_id = int(match.group(1))
-        group_id = event.chat_id  # الحصول على معرف المجموعة الحالية
-        # محاولة حذف الكروب من قاعدة البيانات
+        group_id = event.chat_id
         result = delete_notification_group(group_id, notification_group_id)
         if result:
             await event.reply(f"تم حذف الكروب بمعرف: {notification_group_id} من قاعدة البيانات.")
@@ -52,16 +57,14 @@ async def handle_edited_message(event):
             f"🆔 **الايدي**: `{sender_id}`\n"
             f"📎 [رابط الرسالة المعدلة]({message_link})"
         )
-
         notification_group_id = get_notification_group(event.chat_id)
         if notification_group_id:
-            # إرسال البلاغ فورًا إلى كروب التبليغ
             try:
                 await event.client.send_message(notification_group_id, report_text, link_preview=False)
                 await event.client.send_message(event.chat_id, "تم إبلاغ المشرفين في كروب التبليغ.")
             except Exception as e:
                 await event.client.send_message(event.chat_id, f"تعذر إرسال البلاغ إلى كروب التبليغ: {str(e)}")
         else:
-            await event.client.send_message(event.chat_id, "لم يتم تعيين كروب التبليغ لهذه المجموعة. استخدم الأمر 'اضف كروب <معرف>' لتعيين كروب التبليغ.")
+            await event.client.send_message(event.chat_id, "لم يتم تعيين كروب التبليغ لهذه المجموعة. استخدم الأمر 'اضف كروب <معرف>' لتعيينه.")
 
 ABH.run_until_disconnected()

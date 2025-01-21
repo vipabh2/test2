c0a8a4a99064b07c5054e517beb3d2c98484c2d6
VIPABH <a891983.2@gmail.com>
2025-01-22 01:18:36 +0300

    Update p.py

--- p.py
+++ p.py
@@ -5,7 +5,7 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
-@ABH.on(events.EditedMessage)
+@ABH.on(events.MessageEdited)
 async def echo(event):
     await event.reply("ها ههههه سالمين")
     

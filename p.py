a732a7f1561f213a3d794e54400326b6cef5c356
VIPABH <a891983.2@gmail.com>
2025-01-22 01:24:01 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,14 +1,17 @@
 from telethon import TelegramClient, events
 import os
 
-api_id = os.getenv('API_ID')
+api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
+
 @ABH.on(events.MessageEdited)
 async def echo(event):
-    if event.message == media:
+    if event.message.media:  
         await event.reply("ها ههههه سالمين")
     else:
-        return    
+        return
+
 ABH.run_until_disconnected()

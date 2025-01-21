51ad40e3465a4891ce89019f5833fc449def4346
VIPABH <a891983.2@gmail.com>
2025-01-22 02:38:01 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import create_engine
-import os
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if not DATABASE_URL:
-    raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# if not DATABASE_URL:
+#     raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
 
-if DATABASE_URL.startswith('postgresql://'):
-    engine = create_engine(DATABASE_URL, echo=False)
-    BASE = declarative_base()
-    SessionLocal = sessionmaker(bind=engine)
-    SESSION = SessionLocal()
+# if DATABASE_URL.startswith('postgresql://'):
+#     engine = create_engine(DATABASE_URL, echo=False)
+#     BASE = declarative_base()
+#     SessionLocal = sessionmaker(bind=engine)
+#     SESSION = SessionLocal()
 
-else:
-    raise ValueError("رابط قاعدة البيانات غير صالح أو غير مدعوم. تأكد من أنه PostgreSQL.")
+# else:
+#     raise ValueError("رابط قاعدة البيانات غير صالح أو غير مدعوم. تأكد من أنه PostgreSQL.")

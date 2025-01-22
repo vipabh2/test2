006a668ceb4ebbf6cc7b038537a3e56c667df94c
VIPABH <a891983.2@gmail.com>
2025-01-22 14:44:16 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,9 +9,7 @@ bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 create_table()
 
-from telethon.tl.types import ChatAdminRights
 
-# إضافة أدمن (إما المالك أو صاحب الـ ID المحدد يمكنه ذلك)
 @ABH.on(events.NewMessage(pattern='ارفع'))
 async def add_admin_command(event):
     if event.is_group:
@@ -34,7 +32,6 @@ async def add_admin_command(event):
     else:
         return
 
-# دالة للتحقق إذا كان الشخص مالك المجموعة
 async def is_owner(event):
     participant = await event.get_chat()
     
@@ -43,7 +40,27 @@ async def is_owner(event):
     if permissions.is_creator:
         return True
     return False
-
+@ABH.on(events.NewMessage(pattern='نزل'))
+async def remove_admin_command(event):
+    if event.is_group:
+        user_id = event.sender_id  # ID المستخدم الذي أرسل الأمر
+        # تحقق إذا كان الشخص هو مالك المجموعة أو صاحب الـ ID المحدد
+        if event.sender_id == 1910015590 or await is_owner(event):  # إذا كان المالك أو صاحب الـ ID
+            if event.is_reply:
+                reply_message = await event.get_reply_message()
+                user_id_to_remove = reply_message.sender_id
+                user = reply_message.sender
+                try:
+                    remove_admin(user_id_to_remove)
+                    await event.reply(f"✅ تم إزالة المستخدم {user.first_name} من الأدمن.")
+                except Exception as e:
+                    await event.reply(f"❌ خطأ أثناء إزالة الأدمن: {str(e)}")
+            else:
+                await event.reply("❗ يرجى الرد على رسالة المستخدم الذي تريد إزالة صلاحيات الأدمن عنه.")
+        else:
+            await event.reply("❌ ليس لديك صلاحية لإجراء هذه العملية. فقط المالك أو صاحب الـ ID المحدد يمكنه إزالة أدمن.")
+    else:
+        return
 @ABH.on(events.NewMessage(pattern='سماح'))
 async def approve_user(event):
     if event.is_group:

0f06f738372bdfc264e573107e4e90588ac1ff8e
VIPABH <a891983.2@gmail.com>
2025-01-22 23:09:06 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,4 +1,4 @@
-from db import add_approved_user, remove_approved_user, get_approved_users, create_table
+from db import add_approved_user, remove_approved_user, get_approved_users, ‎recreate_tables‎
 from telethon.tl.types import ChatAdminRights
 from telethon import TelegramClient, events
 import os
@@ -58,7 +58,7 @@ async def remove_admin_command(event):
     else:
         return
 
-create_table()
+‎recreate_tables‎()
 
 @ABH.on(events.NewMessage(pattern='سماح'))
 async def approve_user(event):

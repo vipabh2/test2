df076318a9c82cc23b40a03ebd44f4af070ca5cf
VIPABH <a891983.2@gmail.com>
2025-01-22 15:19:06 +0300

    Update p.py

--- p.py
+++ p.py
@@ -6,7 +6,6 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
-create_table()
 
 
 @ABH.on(events.NewMessage(pattern='ارفع'))

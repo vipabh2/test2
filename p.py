07c98ff9ce7dc73f5d7496bb2df9718b971dda42
VIPABH <a891983.2@gmail.com>
2025-01-23 01:57:40 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,4 +1,4 @@
-from db import ApprovedUser, get_approved_users, remove_approved_user, is_approved_user
+from db import ApprovedUser, get_approved_users, remove_approved_user, is_approved_user, init_db
 from telethon import TelegramClient, events
 import os
 

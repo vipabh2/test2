302e3b818546b97410281ceeca3c06c47e691fa9
VIPABH <a891983.2@gmail.com>
2025-01-22 03:36:10 +0300

    Update db.py

--- db.py
+++ db.py
@@ -15,11 +15,8 @@ class Approval(BASE):
     __tablename__ = 'approvals'
     user_id = Column(Integer, primary_key=True)
 
-# إنشاء الجداول
 def create_table():
     BASE.metadata.create_all(bind=engine)
-
-# إضافة مستخدم إلى قائمة المسموح لهم
 def add_approved_user(user_id):
     db_session = SessionLocal()
     new_user = Approval(user_id=user_id)
@@ -27,7 +24,6 @@ def add_approved_user(user_id):
     db_session.commit()
     db_session.close()
 
-# إزالة مستخدم من قائمة المسموح لهم
 def remove_approved_user(user_id):
     db_session = SessionLocal()
     user = db_session.query(Approval).filter(Approval.user_id == user_id).first()
@@ -36,9 +32,15 @@ def remove_approved_user(user_id):
         db_session.commit()
     db_session.close()
 
-# استرجاع قائمة المستخدمين المسموح لهم
 def get_approved_users():
     db_session = SessionLocal()
     users = db_session.query(Approval).all()
     db_session.close()
     return [user.user_id for user in users]
+    
+def add_group(group_id, group_name):
+    db_session = SessionLocal()
+    new_group = Group(group_id=group_id, group_name=group_name)
+    db_session.add(new_group)
+    db_session.commit()
+    db_session.close()

55c974ea8badf43d294acaf1ebd0646ff66c9f75
VIPABH <a891983.2@gmail.com>
2025-01-22 14:12:58 +0300

    Update db.py

--- db.py
+++ db.py
@@ -7,18 +7,56 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
 
+# إعداد قاعدة البيانات
 engine = create_engine(DATABASE_URL, echo=False)
 BASE = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 
+# تعريف جدول الأدمن
+class Admin(BASE):
+    __tablename__ = 'admins'
+    user_id = Column(BigInteger, primary_key=True)
+
+# تعريف جدول الموافقات
 class Approval(BASE):
     __tablename__ = 'approvals'
-    user_id = Column(BigInteger, primary_key=True)  # تغيير من Integer إلى BigInteger
+    user_id = Column(BigInteger, primary_key=True)
 
+# تعريف جدول المجموعات
+class Group(BASE):
+    __tablename__ = 'groups'
+    group_id = Column(BigInteger, primary_key=True)
+    group_name = Column(String, index=True)
+
+# إنشاء الجداول في قاعدة البيانات
 def create_table():
-    # BASE.metadata.drop_all(bind=engine)
-    BASE.metadata.create_all(bind=engine)  # إعادة إنشاء الجدول
+    BASE.metadata.create_all(bind=engine)
 
+# إضافة أدمن إلى قاعدة البيانات
+def add_admin(user_id):
+    db_session = SessionLocal()
+    new_admin = Admin(user_id=user_id)
+    db_session.add(new_admin)
+    db_session.commit()
+    db_session.close()
+
+# إزالة أدمن من قاعدة البيانات
+def remove_admin(user_id):
+    db_session = SessionLocal()
+    admin = db_session.query(Admin).filter(Admin.user_id == user_id).first()
+    if admin:
+        db_session.delete(admin)
+        db_session.commit()
+    db_session.close()
+
+# التحقق من كون المستخدم أدمن
+def is_admin(user_id):
+    db_session = SessionLocal()
+    admin = db_session.query(Admin).filter(Admin.user_id == user_id).first()
+    db_session.close()
+    return admin is not None
+
+# إضافة مستخدم إلى قائمة الموافقات
 def add_approved_user(user_id):
     if not isinstance(user_id, int):
         raise ValueError(f"المعرّف {user_id} ليس رقمًا صحيحًا.")
@@ -28,7 +66,7 @@ def add_approved_user(user_id):
     db_session.commit()
     db_session.close()
 
-
+# إزالة مستخدم من قائمة الموافقات
 def remove_approved_user(user_id):
     db_session = SessionLocal()
     user = db_session.query(Approval).filter(Approval.user_id == user_id).first()
@@ -37,12 +75,14 @@ def remove_approved_user(user_id):
         db_session.commit()
     db_session.close()
 
+# الحصول على قائمة المستخدمين المسموح لهم
 def get_approved_users():
     db_session = SessionLocal()
     users = db_session.query(Approval).all()
     db_session.close()
     return [user.user_id for user in users]
 
+# إضافة مجموعة إلى قاعدة البيانات
 def add_group(group_id, group_name):
     db_session = SessionLocal()
     new_group = Group(group_id=group_id, group_name=group_name)

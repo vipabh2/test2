455b28e29f297ff90de9389eef29217ed673f6a9
VIPABH <a891983.2@gmail.com>
2025-01-22 13:30:13 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 import os
 
-# إعداد قاعدة البيانات
 DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("متغير البيئة 'DATABASE_URL' غير موجود. تأكد من تعيينه بشكل صحيح.")
@@ -12,18 +11,10 @@ engine = create_engine(DATABASE_URL, echo=False)
 BASE = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 
-# تعريف جدول الموافقات
 class Approval(BASE):
     __tablename__ = 'approvals'
-    user_id = Column(BigInteger, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)  # استخدام BigInteger هنا
 
-# تعريف جدول المجموعات
-class Group(BASE):
-    __tablename__ = 'groups'
-    group_id = Column(BigInteger, primary_key=True)
-    group_name = Column(String, nullable=False)
-
-# إنشاء الجداول
 def create_table():
     BASE.metadata.create_all(bind=engine)
 
@@ -51,10 +42,16 @@ def get_approved_users():
     db_session.close()
     return [user.user_id for user in users]
 
-# إضافة مجموعة
-def add_group(group_id, group_name):
-    db_session = SessionLocal()
-    new_group = Group(group_id=group_id, group_name=group_name)
-    db_session.add(new_group)
-    db_session.commit()
-    db_session.close()
+# إضافة مستخدم جديد
+def add_approved_user(user_id):
+    try:
+        db_session = SessionLocal()
+        new_user = Approval(user_id=user_id)
+        db_session.add(new_user)
+        db_session.commit()
+    except Exception as e:
+        db_session.rollback()
+        raise ValueError(f"خطأ أثناء إضافة المستخدم: {e}")
+    finally:
+        db_session.close()
+

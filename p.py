5738a1ca210ffeaba8765faa505a50ce3623e766
VIPABH <a891983.2@gmail.com>
2025-01-26 20:54:35 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,6 +9,8 @@ ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 head = None
 tail = None
+p1 = None
+p2 = None
 
 @ABH.on(events.NewMessage(pattern='/fliby'))
 async def fliby(event):
@@ -16,11 +18,13 @@ async def fliby(event):
     sender = await event.get_sender()
     p1 = event.sender_id
     n1 = sender.first_name
-    await event.reply(f"عزيزي {n1} تم تسجيلك في لعبة فليبي ")
+    
+    await event.reply(f"عزيزي {n1} تم تسجيلك في لعبة فليبي.")
     await asyncio.sleep(3)
-    await event.edit(
-        f"عزيزي {n1} تم تسجيلك في لعبة فليبي \n انتظر حتى يتم تسجيل الاعب الاخر",
-        buttons=[[Button.inline("انا", b"pl2")]]
+    
+    await event.respond(
+        f"عزيزي {n1} تم تسجيلك في لعبة فليبي.\nانتظر حتى يتم تسجيل اللاعب الآخر.",
+        buttons=[[Button.inline("أنا", b"pl2")]]
     )
 
 @ABH.on(events.CallbackQuery(data=b"pl2"))
@@ -29,14 +33,12 @@ async def pl2(event):
     sender = await event.get_sender()
     p2 = event.sender_id
     n2 = sender.first_name
+    
     head = random.choice([p1, p2])
     tail = p1 if head == p2 else p2
-    await event.respond(f"عزيزي {n2} تم تسجيلك في لعبة فليبي ")
+    
+    await event.respond(f"عزيزي {n2} تم تسجيلك في لعبة فليبي.")
     await asyncio.sleep(3)
-    await event.edit(
-        f"عزيزي {n2} تم تسجيلك في لعبة فليبي \n انتظر حتى يتم تسجيل الاعب الاخر"
-    )
-    await asyncio.sleep(3)
-    await event.respond(f"الاعب الاول هو {head} والاعب الثاني هو {tail}")
+    await event.respond(f"اللاعب الأول هو {head} واللاعب الثاني هو {tail}.")
 
 ABH.run_until_disconnected()

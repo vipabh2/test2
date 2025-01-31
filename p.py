7fb62afe3d9e14c570225ffd07979ea50bb15946
VIPABH <a891983.2@gmail.com>
2025-02-01 01:49:09 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,10 +4,12 @@ import os, random
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
+
 @ABH.on(events.NewMessage(pattern='/start$'))
 async def start(event):
     rl = random.randint(2, 273)
     url = f"https://t.me/IUABH/{rl}"
-    await event.send_file(url)
+    await event.send_file(event.chat_id, url, caption="📹")
 ABH.run_until_disconnected()

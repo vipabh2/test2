596b52f7e89676726cb98ad864c15f7c4639a5e7
VIPABH <a891983.2@gmail.com>
2025-02-01 01:54:31 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,13 +4,21 @@ import os, random
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-
+cap = [
+    "ههههههه",
+    "راويني السنون الصفر",
+    "اضحك شبيك شايل الدنية علئ اجتافك",
+    "صح سخيفة بس هههه حلوه",
+    "انت طلبت الامر ليش ما ضحكت",
+    "😂",
+    "يسعدلي مسائك😀"
+]
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
-
 @ABH.on(events.NewMessage(pattern='/start$'))
 async def start(event):
+    global cap
     rl = random.randint(2, 273)
     url = f"https://t.me/IUABH/{rl}"
-    await ABH.send_file(event.chat_id, url, caption="📹")
+    await ABH.send_file(event.chat_id, url, caption=f"{cap}")
 
 ABH.run_until_disconnected()

fb1d6525eabca8f4b43484a7aa265cdb5e95d975
VIPABH <a891983.2@gmail.com>
2025-02-01 03:19:38 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,17 +1,14 @@
 from telethon import TelegramClient, events
 import os, random 
 
-# تحميل بيانات API
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 
-# تشغيل البوت
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
 @ABH.on(events.NewMessage(pattern='/start'))
 async def reply_abh(event):
-    # if event.chat_id == -1001968219024:
     iu = random.choice(["ch1", "ch2"])  
     if iu == "ch1":  
         rl = random.randint(1222, 1223)
@@ -19,9 +16,8 @@ async def reply_abh(event):
     else:  
         rl = random.randint(242, 255)
         url = f"https://t.me/iuABH/{rl}"  
-        caption = "ابن هاشم (رض) مرات متواضع ،🌚 @K_4x1"
-        await event.client.send_file(event.chat_id, url, caption=caption, reply_to=event.message.id)   
-    else:
-        return
-        
+
+    caption = "ابن هاشم (رض) مرات متواضع ،🌚 @K_4x1"
+    await event.client.send_file(event.chat_id, url, caption=caption, reply_to=event.message.id)   
+
 ABH.run_until_disconnected()

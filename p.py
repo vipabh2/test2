600457ec2586e23e1ae6a5ef39ee7f6f5173bd5d
VIPABH <a891983.2@gmail.com>
2025-02-02 15:35:23 +0300

    Update p.py

--- p.py
+++ p.py
@@ -8,16 +8,51 @@ bot_token = os.getenv('BOT_TOKEN')
 
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
-@ABH.on(events.NewMessage(pattern='^/satrt$'))
+@ABH.on(events.NewMessage(pattern='^رمضان$'))
 async def cunt(event):
     t = datetime.datetime.today()
     t2 = datetime.date(2025, 3, 1)
     days_difference = (t2 - t.date()).days
-    
     if days_difference < 0:
-        await event.reply("The date has passed!")
+        await event.reply("الشهر قد بدأ \n يا مطوري حدث الكود @k_4x1")
     else:
-        await event.reply(f"Days remaining: {days_difference} days")
+        await event.reply(f"باقي {days_difference} ايام")
+@ABH.on(events.NewMessage(pattern='^رجب$'))
+async def cunt(event):
+    t = datetime.datetime.today()
+    t2 = datetime.date(2025, 12, 21)
+    days_difference = (t2 - t.date()).days
+    if days_difference < 0:
+        await event.reply("الشهر قد بدأ \n يا مطوري حدث الكود @k_4x1")
+    else:
+        await event.reply(f"باقي {days_difference} ايام")
+@ABH.on(events.NewMessage(pattern='^شعبان$'))
+async def cunt(event):
+    t = datetime.datetime.today()
+    t2 = datetime.date(2026, 2, 2)
+    days_difference = (t2 - t.date()).days
+    if days_difference < 0:
+        await event.reply("الشهر قد بدأ \n يا مطوري حدث الكود @k_4x1")
+    else:
+        await event.reply(f"باقي {days_difference} ايام")
+@ABH.on(events.NewMessage(pattern='^رمضان$'))
+async def cunt(event):
+    t = datetime.datetime.today()
+    t2 = datetime.date(2025, 3, 1)
+    days_difference = (t2 - t.date()).days
+    if days_difference < 0:
+        await event.reply("الشهر قد بدأ \n يا مطوري حدث الكود @k_4x1")
+    else:
+        await event.reply(f"باقي {days_difference} ايام")
+@ABH.on(events.NewMessage(pattern='^محرم$'))
+async def cunt(event):
+    t = datetime.datetime.today()
+    t2 = datetime.date(2025, 6, 26)
+    days_difference = (t2 - t.date()).days
+    if days_difference < 0:
+        await event.reply("الشهر قد بدأ \n يا مطوري حدث الكود @k_4x1")
+    else:
+        await event.reply(f"باقي {days_difference} ايام")
         
 print("Bot is running...")
 ABH.run_until_disconnected()

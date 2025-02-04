785281f1063564314390db5cefab6f353d579fb3
VIPABH <a891983.2@gmail.com>
2025-02-05 02:46:18 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,7 +4,7 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
-@ABH.on(events.NewMessage(pattern='ابن هاشم'))
+@ABH.on(events.NewMessage(pattern='/start'))
 async def reply_abh(event):
     if event.chat_id == -1001968219024:
         rl = random.randint(1222, 1241)

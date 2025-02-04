cedb40d8697d471f63bb2b6e5b9c6dcac51127b3
VIPABH <a891983.2@gmail.com>
2025-02-05 02:49:06 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,18 +1,22 @@
 from telethon import TelegramClient, events, Button
 import os, random
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
+
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
+
 @ABH.on(events.NewMessage(pattern='/start'))
 async def reply_abh(event):
     if event.chat_id == -1001968219024:
         rl = random.randint(1222, 1241)
         url = f"https://t.me/VIPABH/{rl}"
         caption = "ابن هاشم (رض) مرات متواضع ،🌚 @K_4x1"
-        button = Button.inline("الking", "https://t.me/K_4x1")
-        await event.client.send_file(event.chat_id, url, caption=caption, reply_to=event.message.id,
-                                     Button=button)    
+        button = [Button.url("الking", "https://t.me/K_4x1")]
+        await event.client.send_file(event.chat_id, url, caption=caption, reply_to=event.message.id, buttons=button)
     else:
         return
+
+print("Bot is running...")
 ABH.run_until_disconnected()

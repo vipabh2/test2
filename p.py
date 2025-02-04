5cae16615f62a0852c22dce9dbc2e2f27debab40
VIPABH <a891983.2@gmail.com>
2025-02-04 15:06:39 +0300

    Update p.py

--- p.py
+++ p.py
@@ -7,20 +7,21 @@ bot_token = os.getenv('BOT_TOKEN')
 
 ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 
-@ABH.on(events.NewMessage(pattern=r"^ارسلs+(.+?)\s+(@\w+)$"))
+@ABH.on(events.NewMessage(pattern=r"^ارسل\s+(.+?)\s+(@\w+)$"))
 async def send_message_to_user(event):
-    message = event.pattern_match.group(1) 
+    message = event.pattern_match.group(1)
     username = event.pattern_match.group(2)
-    reciver = await ABH.get_entity(username)
-    reciver_id = reciver.id
+    receiver = await ABH.get_entity(username)
+    receiver_id = receiver.id
     sender_id = event.sender_id
     await ABH.send_message(
-        reciver_id,
-        f"اكو فد واحد دزلك رسالة بس شخصيتة ضعيفةدزها مخفية \n الرسالة 👇 \n {message}"
-        )
+        receiver_id,
+        f"اكو فد واحد دزلك رسالة بس شخصيتة ضعيفة دزها مخفية \n الرسالة 👇 \n {message}"
+    )
     await ABH.send_message(
         sender_id,
-        f"رسالتك الفارغة دزيتها , مره لخ دزها انت وصير سبع \n دزيتها ل {username}"
-        )
+        f"رسالتك الفارغة دزيتها, مرة لخ دزها انت وصير سبع \n دزيتها ل {username}"
+    )
+
 print("✅ Bot is running...")
 ABH.run_until_disconnected()

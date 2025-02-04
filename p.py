25e363dde58b15f20372fcf765a8b3c3b690c1ab
VIPABH <a891983.2@gmail.com>
2025-02-05 02:38:37 +0300

    Update p.py

--- p.py
+++ p.py
@@ -7,7 +7,7 @@ ABH = TelegramClient('c', api_id, api_hash).start(bot_token=bot_token)
 @ABH.on(events.NewMessage(pattern='حجرة'))
 async def rp(event):
     sender = event.sender_id
-    name = event.sender_fullname
+    name = event.sender_name
     Button = Button.inline("العب", b"join")
     await event.reply(f"عزيزي {name} تم تسجيلك في اللعبة \n انتظر الاعب الاخر",
                       Button=Button)

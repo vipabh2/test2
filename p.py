14118bccf590848c4caab479234cd5ceb70c4293
VIPABH <a891983.2@gmail.com>
2025-02-09 01:07:54 +0300

    Update p.py

--- p.py
+++ p.py
@@ -50,9 +50,6 @@ async def tmuter(event):
 
     # التحقق من أن المستخدم لديه صلاحية تقييد الأعضاء
     sender = await event.get_sender()
-    if not sender.admin_rights or not sender.admin_rights.ban_users:
-        return await event.edit("❌ ليس لديك صلاحية لتقييد الأعضاء.")
-
     # الحصول على المستخدم الذي تم الرد عليه
     replied_message = await event.get_reply_message()
     if not replied_message:

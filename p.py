cd0a7a517e9290ec61b9d4aa918919570b15c742
VIPABH <a891983.2@gmail.com>
2025-02-09 02:56:41 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,15 +9,13 @@ client = TelegramClient('session_name', api_id, api_hash)
 
 @client.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
 async def permalink(mention):
-    """Generates a link to the user's PM with a custom text."""
-    user, custom = mention.pattern_match.group(1)
-    if not user:
-        return
-    if custom:
-        return await mention.reply(mention, f"[{custom}](tg://user?id={user.id})")
-    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
-    await mention.reply(mention, f"⌔︙[{tag}](tg://user?id={user.id})")
-
+    """Generates a link to the user's PM based on the ID."""
+    user_id = mention.pattern_match.group(1)  # استخراج رقم الـ ID من الرسالة
+    try:
+        # إرسال رابط الحساب مباشرة باستخدام الـ ID
+        await mention.reply(f"⌔︙[رابط الحساب](tg://user?id={user_id})")
+    except Exception as e:
+        await mention.reply("حدث خطأ أثناء محاولة العثور على المستخدم.")  # في حال حدوث مشكلة
 
 client.start()
 client.run_until_disconnected()

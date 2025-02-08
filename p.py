70e696673568fc15b7552c328791152fe79c3912
VIPABH <a891983.2@gmail.com>
2025-02-09 02:30:45 +0300

    Update p.py

--- p.py
+++ p.py
@@ -7,13 +7,19 @@ bot_token = os.getenv('BOT_TOKEN')
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-@client.on(events.UserUpdate)
-async def handler(event):
-    # مراقبة الحظر أو رفع الحظر
-    if event.is_blocked:
-        print(f"تم حظر عضو: {event.user_id}")
-    else:
-        print(f"تم رفع الحظر عن عضو: {event.user_id}")
+@client.on(events.NewMessage)
+async def permalink(mention):
+    """Generates a link to the user's PM with a custom text."""
+        # user, custom = await get_user_from_event(mention)
+    # if not user:
+    #     return
+    # if custom:
+        # return
+    user = mention.pattern_match.group(0)
+    await mention.reply(mention, f"[{custom}](tg://user?id={user.id})")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    await mention.reply(mention, f"⌔︙[{tag}](tg://user?id={user.id})")
+
 
 client.start()
 client.run_until_disconnected()

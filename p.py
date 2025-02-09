9fb0f8c7dd90ef2473aedd52b69f093f1c4c8e60
VIPABH <a891983.2@gmail.com>
2025-02-09 21:26:23 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,21 +1,31 @@
 from telethon import TelegramClient, events
+from telethon.tl.types import KeyboardButtonCallback
 import os
 
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 
-client = TelegramClient('session_name', api_id, api_hash)
+ABH = TelegramClient('session_name', api_id, api_hash)
 
-@client.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
-async def permalink(mention):
-    """Generates a link to the user's PM based on the ID."""
-    user_id = mention.pattern_match.group(1)  # استخراج رقم الـ ID من الرسالة
-    try:
-        # إرسال رابط الحساب مباشرة باستخدام الـ ID
-        await mention.reply(f"⌔︙tg://user?id={user_id} 👈 الرابط")
-    except Exception as e:
-        await mention.reply("حدث خطأ أثناء محاولة العثور على المستخدم.")  # في حال حدوث مشكلة
+@ABH.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
+async def permalink(event):
+    user_id = event.pattern_match.group(1)
+    user = await event.client.get_entity(int(user_id))
+    custom = user.first_name
+    if not user:
+        return await event.reply(f"User not found.")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    button = KeyboardButtonCallback("تغيير الئ رابط", b"recgange")
+    await event.reply(f"⌔︙[{tag}](tg://user?id={user.id})", buttons=[button])
 
-client.start()
-client.run_until_disconnected()
+@ABH.on(events.CallbackQuery(data=b"recgange"))
+async def chang(event):
+    user_id = event.pattern_match.group(1)
+    user = await event.client.get_entity(int(user_id))
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    button = KeyboardButtonCallback("تغيير الئ رابط", b"recgange")
+    await event.edit(f"⌔︙[{tag}](tg://user?id={user.id})", buttons=[button])
+
+ABH.start(bot_token=bot_token)
+ABH.run_until_disconnected()

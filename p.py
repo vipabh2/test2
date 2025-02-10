52cb66d5553830f5cfdcb600bdab02cf713c884d
VIPABH <a891983.2@gmail.com>
2025-02-11 00:28:42 +0300

    Update p.py

--- p.py
+++ p.py
@@ -10,9 +10,7 @@ bot_token = os.getenv('BOT_TOKEN')
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @client.on(events.chataction)
 async def handle_chat_action(event):
-    if event.action is not None:
-        print(event.action)
-        
-
+    if event.user_restricted is not None:
+        await client.send_message(event.user_restricted, "تم حظرك من البوت")
 
 client.run_until_disconnected()

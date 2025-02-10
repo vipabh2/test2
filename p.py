4b8be3dde3f405128aeb38acb7ed98cc5832bece
VIPABH <a891983.2@gmail.com>
2025-02-10 16:38:57 +0300

    Update p.py

--- p.py
+++ p.py
@@ -51,7 +51,7 @@ async def auto_unrestrict(event):
             chat = await event.get_chat()
 
             # إيقاف إرسال الرسائل عند الطرد
-            await client(EditBannedRequest(chat.id, user.id, unrestrict_rights))
+            await client(EditBannedRequest(chat.id, user.id, restrict_rights))
             await event.reply(f"🚫 تم طرد {user.first_name} من المجموعة، ولا يمكنه إرسال الرسائل بعد الآن.")
 
         # التحقق من حالة "user_restricted" (تقييد المستخدم)

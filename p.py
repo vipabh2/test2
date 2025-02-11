0f0e2c1c38ae748580ec65671c643a11e9aff9d0
VIPABH <a891983.2@gmail.com>
2025-02-12 01:03:11 +0300

    Update p.py

--- p.py
+++ p.py
@@ -11,12 +11,16 @@ client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 # تخزين عدد الضغطات لكل زر
 votes = {'button1': 0, 'button2': 0}
-vote_text = ""
+
+# تخزين معرّفات المستخدمين الذين قاموا بالتصويت
+voted_users = set()
 
 @client.on(events.NewMessage(pattern=r'^تصويت\s+(.+)$'))
 async def handler(event):
     global vote_text
-    vote_text = event.pattern_match.group(1)
+    txt = event.pattern_match
+    if txt:
+        vote_text = txt.group(1)
     await event.respond(
         f'{vote_text}',
         buttons=[
@@ -28,11 +32,20 @@ async def handler(event):
 @client.on(events.CallbackQuery)
 async def callback(event):
     data = event.data.decode('utf-8')
+    user_id = event.sender_id  # الحصول على معرّف المستخدم
+
+    # التحقق إذا كان المستخدم قد صوت بالفعل
+    if user_id in voted_users:
+        await event.answer("❌ لقد قمت بالتصويت بالفعل!", alert=True)
+        return
 
     if data == 'button1':
         votes['button1'] += 1
     elif data == 'button2':
         votes['button2'] += 1
+
+    # إضافة المستخدم إلى قائمة الذين قاموا بالتصويت
+    voted_users.add(user_id)
 
     await event.edit(
         f'{vote_text}',

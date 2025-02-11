5c0896da934aef7680936f2bc4ee25dad443402d
VIPABH <a891983.2@gmail.com>
2025-02-12 01:38:36 +0300

    Update p.py

--- p.py
+++ p.py
@@ -12,42 +12,63 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+# تخزين عدد الضغطات لكل زر
 votes = {'button1': 0, 'button2': 0}
+
+# تخزين معرّفات المستخدمين الذين قاموا بالتصويت
 voted_users = set()
+
 @ABH.on(events.NewMessage(pattern=r'^تصويت\s+(.+)$'))
 async def handler(event):
     global vote_text
-    isabh = event.sender_id
+    isabh = event.sender_id  # معرف المرسل
     txt = event.pattern_match
-    if isabh != 1910015590:
+
+    # حذف الرسالة إذا كان المرسل هو نفسه (حسب isabh)
+    if isabh == event.sender_id:
         await event.delete()
         return
+
     if txt:
         vote_text = txt.group(1)
+
+    # إرسال الرسالة مع الأزرار
     await event.respond(
         f'{vote_text} \n `التصويت اما👍 او 👎 لمره واحده`',
         buttons=[
             [Button.inline(f'👍 {votes["button1"]}', data='button1')],
             [Button.inline(f'👎 {votes["button2"]}', data='button2')]
-        ])
+        ]
+    )
+
 @ABH.on(events.CallbackQuery)
 async def callback(event):
     data = event.data.decode('utf-8')
-    user_id = event.sender_id
+    user_id = event.sender_id  # الحصول على معرّف المستخدم
+
+    # التحقق إذا كان المستخدم قد صوت بالفعل
     if user_id in voted_users:
         await event.answer("الملحة متفيدك كبدي , التصويت لمره واحده🙂", alert=True)
         return
+
     if data == 'button1':
         votes['button1'] += 1
     elif data == 'button2':
         votes['button2'] += 1
+
+    # إضافة المستخدم إلى قائمة الذين قاموا بالتصويت
     voted_users.add(user_id)
+
+    # تحديث الرسالة بالأزرار مع العد الجديد
     await event.edit(
         f'{vote_text} \n `التصويت لمره واحده`',
         buttons=[
             [Button.inline(f'👍 {votes["button1"]}', data='button1')],
             [Button.inline(f'👎 {votes["button2"]}', data='button2')]
-        ])
+        ]
+    )
+
+ABH.run_until_disconnected()
 @ABH.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
 async def permalink(event):
     global user, uid

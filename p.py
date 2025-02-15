c3b38a0ed409949d9c0201226b35594b84fc1d91
VIPABH <a891983.2@gmail.com>
2025-02-16 01:18:56 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,27 +1,12 @@
-import asyncio, os
+import os
 from telethon import TelegramClient, events
-from telethon.tl.types import ChatBannedRights
 
-# تحميل API_ID و API_HASH و BOT_TOKEN من البيئة
-api_id = os.getenv('API_ID')      
+api_id = int(os.getenv('API_ID'))      
 api_hash = os.getenv('API_HASH')  
-bot_token = os.getenv('BOT_TOKEN')
-
-# إعداد عميل Telethon
-client = TelegramClient('session_name', api_id, api_hash)
-from telethon import events
-from telethon.tl.types import UpdateChatParticipantAdmin, ChatBannedRights
-
-@client.on(events.Raw)
-async def handler(event):
-    if isinstance(event, UpdateChatParticipantAdmin):
-        user_id = event.user_id
-        chat_id = event.chat_id
-        rights = event.banned_rights
-
-        if isinstance(rights, ChatBannedRights):
-            if rights.send_messages:
-                print(f"🚨 المستخدم {user_id} فقد صلاحية الإرسال في المجموعة {chat_id}")
-
-client.start()
-client.run_until_disconnected()
+bot_token = os.getenv('BOT_TOKEN') 
+ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+@ABH.on(events.NewMessage(pattern='/start'))
+async def sendf(event):
+    await event.reply(file="https://t.me/VIPABH/1242")
+    return
+ABH.run_until_disconnected()

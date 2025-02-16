0233a9afd954feb0412c207b80ecfb22eea3ec38
VIPABH <a891983.2@gmail.com>
2025-02-17 01:08:20 +0300

    Update p.py

--- p.py
+++ p.py
@@ -2,6 +2,7 @@ import requests, os, operator, asyncio, random, uuid, datetime
 from telethon import TelegramClient, events, Button
 from telethon.errors import BadRequestError
 from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
 
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')
@@ -9,8 +10,16 @@ api_hash = os.getenv('API_HASH')
 # إنشاء الجلسة
 ABH = TelegramClient("ubot", api_id, api_hash)
 
-def media_type(message):
-    return None
+@ABH.on(events.NewMessage(pattern="قنواتي$"))
+async def _(event):
+    "To list all public channels and groups."
+    result = await event.client(GetAdminedPublicChannelsRequest())
+    output_str = "᯽︙ جميع القنوات والمجموعات التي قمت بأنشائها :\n"
+    output_str += "".join(
+        f" - {channel_obj.title} @{channel_obj.username} \n"
+        for channel_obj in result.chats
+    )
+    await event.edit(output_str)
 
 @ABH.on(events.NewMessage(pattern="الاحداث( -ر)?(?: |$)(\d*)?"))
 async def iundlt(event):

93ffec2b41256a0a04ef805641d0fe5428758d94
VIPABH <a891983.2@gmail.com>
2025-02-16 23:01:22 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,34 +1,57 @@
 import requests, os, operator, asyncio, random, uuid, datetime 
 from telethon import TelegramClient, events, Button
-from hijri_converter import Gregorian
-from telethon.tl.custom import Button
-from googletrans import Translator
+from telethon.errors import BadRequestError
+from telethon.tl.functions.users import GetFullUserRequest
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-     
-translator = Translator()
-@ABH.on(events.NewMessage(pattern=r'(ترجمة|ترجمه)'))
-async def handle_message(event):
-    if event.is_reply:
-        replied_message = await event.get_reply_message()
-        original_text = replied_message.text
+
+@ABH.on(events.NewMessage(pattern="الاحداث( -ر)?(?: |$)(\d*)?"))
+async def iundlt(event):
+    uid = event.sender_id
+    await event.reply(event, "᯽︙ يـتم الـبحث عن اخـر الاحداث")
+    flag = event.pattern_match.group(1)
+    if event.pattern_match.group(2) != "":
+        lim = int(event.pattern_match.group(2))
+        if lim > 15:
+            lim = int(15)
+        if lim <= 0:
+            lim = int(1)
     else:
-        command_parts = event.message.text.split(' ', 1)
-        original_text = command_parts[1] if len(command_parts) > 1 else None
-    if not original_text:
-        await event.reply("عزيزي ...\n يرجئ كتابة النص مع الامر او بالرد عليه.")
-        return
-    detected_language = translator.detect(original_text).lang
-    if detected_language == "ar":
-        translated = translator.translate(original_text, dest="en")
+        lim = int(5)
+    adminlog = await event.client.get_admin_log(
+        event.chat_id, limit=lim, edit=False, delete=True
+    )
+    deleted_msg = f"᯽︙ اخـر {lim} رسـائل مـحذوفة فـي الـدردشة :"
+    if not flag:
+        for msg in adminlog:
+            ruser = (
+                await event.client(GetFullUserRequest(msg.old.from_id.user_id))
+            ).users[0]
+            media_type = media_type(msg.old)
+            if _media_type is None:
+                deleted_msg += f"\n {msg.old.message} \n ᯽︙ تم ارسالها بـواسطة {_format.mentionuser(ruser.first_name ,ruser.id)}"
+            else:
+                deleted_msg += f"\n {_media_type} \n ᯽︙ ارسلت بـواسطـة {_format.mentionuser(ruser.first_name ,ruser.id)}"
+        await event.reply(uid, deleted_msg)
     else:
-        translated = translator.translate(original_text, dest="ar")
-    response = (
-        f" **اللغة المكتشفة:** `{detected_language}`\n"
-        f" **النص المترجم:** `{translated.text}`"
-        )
-    await event.reply(response)
+        main_msg = await event.reply(uid, deleted_msg)
+        for msg in adminlog:
+            ruser = (
+                await event.client(GetFullUserRequest(msg.old.from_id.user_id))
+            ).users[0]
+            _media_type = media_type(msg.old)
+            if _media_type is None:
+                await main_msg.reply(
+                    f"{msg.old.message}\n᯽︙ ارسلت بواسطه {_format.mentionuser(ruser.first_name ,ruser.id)}"
+                )
+            else:
+                await main_msg.reply(
+                    f"{msg.old.message}\n᯽︙ ارسلت بواسطه {_format.mentionuser(ruser.first_name ,ruser.id)}",
+                    file=msg.old.media,
+                )
+
 
 ABH.run_until_disconnected()

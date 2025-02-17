83e5b81ef43b0229610aed5ff5df14e16cceda02
VIPABH <a891983.2@gmail.com>
2025-02-17 23:46:00 +0300

    Update p.py

--- p.py
+++ p.py
@@ -52,39 +52,6 @@ async def delete_filtered_messages(event):
     except Exception as e:
         # التعامل مع الأخطاء
         await event.reply(f"حدث خطأ أثناء الحذف: {str(e)}")
-@ABH.on(events.NewMessage(pattern="امسح صور$"))
-async def delete_filtered_messages(event):
-    """حذف الرسائل بناءً على فلاتر محددة."""
-
-    await event.delete() 
-
-    try:
-        filters = {
-            "الصور": InputMessagesFilterPhotos
-        }
-
-        total_deleted = 0 
-        deleted_counts = {key: 0 for key in filters.keys()}  
-
-
-        for msg_type, msg_filter in filters.items():
-            async for message in event.client.iter_messages(event.chat_id, filter=msg_filter):
-                if message.sender_id in excluded_user_ids:
-                    continue 
-                if message:
-                    await message.delete()
-                    deleted_counts[msg_type] += 1
-                    total_deleted += 1
-
-        if total_deleted > 0:
-            details = "\n".join([f"{msg_type}: {count}" for msg_type, count in deleted_counts.items() if count > 0])
-            await event.reply(f"تم حذف {total_deleted} رسالة.\nالتفاصيل:\n{details}")
-        else:
-            await event.reply("لا توجد رسائل تطابق الفلاتر المحددة!")
-
-    except Exception as e:
-        await event.reply(f"حدث خطأ أثناء الحذف: {str(e)}")
-
 @ABH.on(events.NewMessage(pattern="قنواتي$"))
 async def list_channels(event):
     """لإظهار جميع القنوات والمجموعات التي قمت بإنشائها."""

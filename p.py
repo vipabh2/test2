59d2a5687bc771d3b2a3b97f70e12bd0868f9462
VIPABH <a891983.2@gmail.com>
2025-02-26 01:30:02 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,24 +1,22 @@
 from telethon import TelegramClient, events, Button
 import random
 import os
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
+
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-choices = [
-    "حجره",
-    "ورقة",
-    "مقص"
-    ]
+
+choices = ["حجره", "ورقة", "مقص"]
+
 @ABH.on(events.NewMessage(pattern="حجرة|/rock"))
-async def start(event):
+async def game_handler(event):
     button = [
         [Button.inline("🪨", b"rock")],
         [Button.inline("✂️", b"cuter")],
         [Button.inline("📜", b"paper")]
-              ]
-    await event.respond("اهلا صديقي اختر احد الاختيارات", Button=button)
-
-abh = random.choice(choices)
+    ]
+    await event.respond("اهلا صديقي، اختر أحد الاختيارات:", buttons=button)
 
 ABH.run_until_disconnected()

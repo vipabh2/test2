7380e752450bd334d485c4bd4e63484b03311eed
VIPABH <a891983.2@gmail.com>
2025-02-25 21:50:07 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,7 +4,21 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+is_on = None
+@ABH.on(events.NewMessage(pattern="تفعيل"))
+async def set(event):
+    global is_on
+    await event.reply("تم التفعيل")
+    is_on = True
+@ABH.on(events.NewMessage(pattern="تعطيل"))
+async def set(event):
+    global is_on
+    await event.reply("تم التعطيل")
+    is_on = False
 @ABH.on(events.MessageEdited)
 async def handler(event):
-     await event.reply('تم تعديل مرفق في هذه الرسالة!')
+     if is_on == False:
+         return
+     else:
+          await event.reply('تم تعديل مرفق في هذه الرسالة!')
 ABH.run_until_disconnected()

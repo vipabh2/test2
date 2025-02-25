72b29671aabe73d2392e80d3ec9125f94a1dbedd
VIPABH <a891983.2@gmail.com>
2025-02-26 02:04:30 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,5 @@
 from telethon import TelegramClient, events, Button
-import random
-import os
+import random, os
 
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
@@ -8,35 +7,30 @@ bot_token = os.getenv('BOT_TOKEN')
 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
 
-choices = {
-    "rock": "🪨 حجره",
-    "paper": "📜 ورقة",
-    "cuter": "✂️ مقص"
-}
+choices = {"rock": "🪨حجره", "paper": "📜ورقة", "cuter": "✂️مقص"}
+active_games = {}  
 
 @ABH.on(events.NewMessage(pattern="حجرة|/rock"))
 async def start(event):
-    buttons = [
-        [Button.inline("🪨", b"rock"), Button.inline("✂️", b"cuter"), Button.inline("📜", b"paper")]
-    ]
-    await event.respond("اختر أحد الاختيارات🌚", buttons=buttons)
+    active_games[event.chat_id] = event.sender_id  
+    buttons = [[Button.inline("🪨", b"rock"), Button.inline("✂️", b"cuter"), Button.inline("📜", b"paper")]]
+    await event.respond("اختر أحد الاختيارات 🌚", buttons=buttons)
 
 @ABH.on(events.CallbackQuery())
 async def callback_handler(event):
+    game_owner = active_games.get(event.chat_id)  
+    if game_owner != event.sender_id:
+        await event.answer("اللعبة ما تخصك \n من تدخل في ما لا يعنيه لقي كلام لا يرضيه", alert=True)
+        return  
+
     user_choice = event.data.decode("utf-8")
+    if user_choice not in choices:
+        return  
+
     bot_choice_key = random.choice(list(choices.keys()))
     bot_choice = choices[bot_choice_key]  
-
-    if user_choice == bot_choice_key:
-        result = "🤝 تعادل!"
-    elif (user_choice == "rock" and bot_choice_key == "cuter") or \
-         (user_choice == "paper" and bot_choice_key == "rock") or \
-         (user_choice == "cuter" and bot_choice_key == "paper"):
-        result = "🎉 فزت!"
-    else:
-        result = "😢 خسرت!"
-
-    new_text = f" اختيارك: {choices[user_choice]}\n مخفي: {bot_choice}\n\n{result}"
-    await event.edit(new_text)
+    result = "🤝تعادل" if user_choice == bot_choice_key else "🎉فزت" if (user_choice == "rock" and bot_choice_key == "cuter") or (user_choice == "paper" and bot_choice_key == "rock") or (user_choice == "cuter" and bot_choice_key == "paper") else "😢خسرت"
+    
+    await event.edit(f"اختيارك:{choices[user_choice]}\n مخفي:{bot_choice}\n\n{result}")
 
 ABH.run_until_disconnected()

a799d96e8ee8493fb5d2c08c51ef6d70a71c3717
VIPABH <a891983.2@gmail.com>
2025-02-25 21:34:59 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,22 +1,11 @@
-import google.generativeai as genai
 from telethon import TelegramClient, events
 import os
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-
-
-GEMINI = "AIzaSyA5pzOpKVcMGm6Aek82KoB3Pk94dYg3LX4"
-genai.configure(api_key=GEMINI)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
 @ABH.on(events.NewMessage)
-async def ai_aljoker(event):
-    if event.text and not event.out:
-        try:
-            joker = model.generate_content(event.text.strip())
-            await event.reply(f"**{joker.text}**")
-        except Exception as e:
-            await event.reply(f"صار خطأ: {e}")
+async def handler(event):
+    if event.message.media and event.message.edited:
+        await event.reply('تم تعديل مرفق في هذه الرسالة!')
 ABH.run_until_disconnected()

a82b0625332451e6d216c6a9f8c0896ffe4d9264
VIPABH <a891983.2@gmail.com>
2025-02-25 21:38:07 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,16 +1,11 @@
 from telethon import TelegramClient, events
 import os
-
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
-
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-
-@ABH.on(events.NewMessage)
+@ABH.on(events.MessageEdited)
 async def handler(event):
-    # تحقق من أن الرسالة تحتوي على مرفق وتم تعديلها
-    if event.message.media and event.message.edit_date:
+    if event.message.media and event.message.edited:
         await event.reply('تم تعديل مرفق في هذه الرسالة!')
-
 ABH.run_until_disconnected()

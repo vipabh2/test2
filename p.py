dd9779a5af4df4d5dd03e90f5539f76ef2c5938d
VIPABH <a891983.2@gmail.com>
2025-02-25 21:59:15 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,24 +1,34 @@
+import re
 from telethon import TelegramClient, events
-import os, re
+import os
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
+
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+
 is_on = False
+
 @ABH.on(events.NewMessage(pattern="تفعيل"))
 async def activate(event):
     global is_on
     await event.reply("تم التفعيل")
     is_on = True
+
 @ABH.on(events.NewMessage(pattern="تعطيل"))
 async def deactivate(event):
     global is_on
     await event.reply("تم التعطيل")
     is_on = False
+
 @ABH.on(events.MessageEdited)
 async def handler(event):
     if not is_on:
         return
-    if event.message.media and isinstance(event.message.media, events.MessageMediaDocument) or event.message.text and re.search(r'http[s]?://', event.message.text):
-        await event.reply('تم تعديل مرفق في هذه الرسالة!')        
+    if event.message.media and isinstance(event.message.media, events.MessageMediaDocument):
+        await event.reply('تم تعديل مرفق (ملف) في هذه الرسالة!')
+    elif event.message.text and re.search(r'http[s]?://', event.message.text):
+        await event.reply('تم تعديل رابط في هذه الرسالة!')
+
 ABH.run_until_disconnected()

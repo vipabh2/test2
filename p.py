d350edd5578e83ba47f8612f5dd67e9ce47c221e
VIPABH <a891983.2@gmail.com>
2025-03-03 03:57:30 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,20 +1,18 @@
-from telethon import TelegramClient, events
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 import os
 
 # بيانات البوت
 api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 
-# تشغيل العميل
-ABH = TelegramClient("userbot_session", api_id, api_hash)
-
 async def main():
-    async with TelegramClient('userbot_session', api_id, api_hash) as client:
+    async with TelegramClient(StringSession(), api_id, api_hash) as client:
         # التأكد من تسجيل الدخول
         me = await client.get_me()
         
         # استخراج session string
-        session_string = await client.session.save()
+        session_string = client.session.save()
 
         print("✅ تم استخراج الكود بنجاح!")
         print(f"Session String: {session_string}")

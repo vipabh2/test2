cdd56d575b551e8605db3cd6f57d84250b6ec1e1
VIPABH <a891983.2@gmail.com>
2025-03-06 00:07:12 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,14 +1,11 @@
 from telethon import TelegramClient, events
-import yt_dlp
-import os
+import yt_dlp, os
 import asyncio
 
-# إعداد المتغيرات
 api_id = os.getenv('API_ID')      # API_ID
 api_hash = os.getenv('API_HASH')  # API_HASH
 bot_token = os.getenv('BOT_TOKEN') # BOT_TOKEN
 
-# إنشاء العميل للبوت
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage())
@@ -16,14 +13,14 @@ async def download_video(event):
     url = event.message.text
 
     # التحقق مما إذا كان الرابط من يوتيوب
-    if "youtube.com/watch?v=" in url or "youtu.be/" in url:
+    if "youtube.com/watch?v=" in url:
         await event.reply("جاري تحميل الفيديو...")
 
         # إعدادات yt-dlp
         ydl_opts = {
             "format": "bestvideo+bestaudio",
-            "outtmpl": "موارد/%(title)s.%(ext)s",  # تحديد المجلد الذي سيتم حفظ الفيديو فيه
-            "noplaylist": True  # عدم تحميل القوائم التلقائية
+            "outtmpl": "%(title)s.%(ext)s",
+            "cookies": "موارد/cookies.txt"  # تحديد المسار لملف الكوكيز
         }
 
         # تنزيل الفيديو باستخدام yt-dlp
@@ -34,8 +31,7 @@ async def download_video(event):
             # الرد برسالة عند اكتمال التحميل
             await event.reply("✅ تم تحميل الفيديو بنجاح!")
         except Exception as e:
-            # الرد برسالة في حال حدوث خطأ
-            await event.reply(f"❌ حدث خطأ أثناء تحميل الفيديو: {str(e)}")
+            await event.reply(f"❌ حدث خطأ أثناء تحميل الفيديو: {e}")
 
 # تشغيل البوت للاستماع للرسائل بشكل مستمر
 client.run_until_disconnected()
